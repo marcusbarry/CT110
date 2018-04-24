c914e6b8fad0db2d9a800656987fcf266252b05f
marcusbarry <38699995+marcusbarry@users.noreply.github.com>
2018-04-24 13:02:55 -0400

    Add files via upload

---
+++ P2HW1_DistanceTraveled_Merritt.py
@@ -0,0 +1,11 @@
+#CTI-110
+#P2HW1_DistanceTraveled_MerrittMarcus.phy 
+#Marcus_Merritt
+# Last modified on 04/24/2018
+
+speed = 70
+print("In 6 hours the car will travel"+ str( (speed * 6) ) + "miles" )
+
+print("In 10 hours the car will travel"+ str( (speed * 10) ) + "miles" )
+
+print("In 15 hours the car will travel"+ str( (speed * 15) )  + "miles" )
